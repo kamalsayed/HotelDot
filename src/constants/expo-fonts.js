7e191cecf8d85ed44044e104418a574bb57891af
Kamal <kmatsho63@gmail.com>
2023-11-08 02:07:43 +0200

    Ready to build first APK

--- src/constants/expo-fonts.js
+++ src/constants/expo-fonts.js
@@ -2,8 +2,8 @@ import { useFonts } from '@expo-google-fonts/nunito-sans';
 
 export const loadFonts = () => {
   const [fontsLoaded] = useFonts({
-    NunitoSans_400Regular: require('/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/fonts/static/NunitoSans_10pt-Regular.ttf'),
-    NunitoSans_700Bold: require('/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/fonts/NunitoSans-VariableFont.ttf'),
+    NunitoSans_400Regular: require('../../assets/fonts/static/NunitoSans_10pt-Regular.ttf'),
+    NunitoSans_700Bold: require('../../assets/fonts/NunitoSans-VariableFont.ttf'),
     
   });
 
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,7 @@
 export const ImgPath={
-    logo:require("/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/Group11.png"),
-    vector:require("/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/Vector4.png"),
-    onboard:require("/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/oboarding.jpeg"),
-    hotelImage1:require("/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/hotel1.jpeg"),
-    hotelImage2:require("/home/kamal/WorkSpace/ReactNative-Dev/Expo/HotelDot/assets/hotel2.jpeg"),
+    logo:require("../../assets/Group11.png"),
+    vector:require("../../assets/Vector4.png"),
+    onboard:require("../../assets/oboarding.jpeg"),
+    hotelImage1:require("../../assets/hotel1.jpeg"),
+    hotelImage2:require("../../assets/hotel2.jpeg"),
 }
